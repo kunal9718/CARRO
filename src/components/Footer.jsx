3a19f1b8f7d5055f1ea9e0832c7c758643a27556
kunal9718 <92395743+kunal9718@users.noreply.github.com>
2023-08-15 22:35:59 +0530

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,8 +21,8 @@ function Footer(){
         
         
                 <div className="foot"><ul>SOCIAL</ul>
-                <ul><a className="footer" href="https://www.linkedin.com/in/sahil-gupta-892750197/" ><i class="fab fa-linkedin-in"></i></a></ul>
-                <ul><a href="https://github.com/sahilgupta0712/" className="footer" ><i class="fa fa-github" aria-hidden="true"></i></a></ul>
+                <ul><a className="footer" href="https://www.linkedin.com/in/kunal-garg-3a01b623b/" ><i class="fab fa-linkedin-in"></i></a></ul>
+                <ul><a href="https://github.com/kunal9718/" className="footer" ><i class="fa fa-github" aria-hidden="true"></i></a></ul>
                 </div>
             </div>
         
